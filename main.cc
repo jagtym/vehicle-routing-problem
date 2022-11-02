f7630247d5d79348f9867d80a047a42309c2ce88
jagtym <jagtymxd@gmail.com>
2022-11-02 12:28:34 +0100

    first commit

---
+++ main.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "utilities.h"
+#include "data_handling.h"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        cout << "usage: " << argv[0] << " [dataset_file]" << endl;
+    }
+}
