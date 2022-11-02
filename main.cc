0d2645fdef7638801fcb07d554c8837355eb1201
jagtym <jagtymxd@gmail.com>
2022-11-02 13:11:02 +0100

    file checking

--- main.cc
+++ main.cc
@@ -1,12 +1,28 @@
 #include <iostream>
-
-#include "utilities.h"
-#include "data_handling.h"
+#include <vector>
+#include <string>
+#include <fstream>
 
 using namespace std;
+
+const int TIME_LIMIT = 900;
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
         cout << "usage: " << argv[0] << " [dataset_file]" << endl;
+    }
+
+    const string filename = argv[1];
+    string problem_name;
+    int vehicle_number;
+    int capacity;
+    vector< vector<int> > customers;
+
+    ifstream input_file;
+    input_file.open(filename);
+
+    if (!input_file.is_open()) {
+        cerr << "File not found!" << endl;
+        return 1;
     }
 }
